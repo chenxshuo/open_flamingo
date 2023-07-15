bf39fc05ae92a372475bd2c64100489bd4dd157a
shuochen <shuo.chen@campus.lmu.de>
2023-07-15 12:05:46 +0800

    feat: one eval

--- try/one_eval.py
+++ try/one_eval.py
@@ -8,16 +8,23 @@ from open_flamingo import create_model_and_transforms
 from huggingface_hub import hf_hub_download
 import torch
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(levelname)s:%(asctime)s:%(name)s:%(filename)s:%(lineno)d]\t %(message)s',
+)
+
 logger = logging.getLogger(__name__)
 
 MODEL_DICT_9B = {
     "language": "anas-awadalla/mpt-7b",
-    "flamingo": "openflamingo/OpenFlamingo-9B-vitl-mpt7b"
+    "flamingo": "openflamingo/OpenFlamingo-9B-vitl-mpt7b",
+    "cross_attn_every_n_layers": 4
 }
 
 MODEL_DICT_4B = {
     "language": "togethercomputer/RedPajama-INCITE-Instruct-3B-v1",
-    "flamingo": "openflamingo/OpenFlamingo-4B-vitl-rpj3b-langinstruct"
+    "flamingo": "openflamingo/OpenFlamingo-4B-vitl-rpj3b-langinstruct",
+    "cross_attn_every_n_layers": 2
 }
 MODEL = MODEL_DICT_4B
 
@@ -26,7 +33,7 @@ model, image_processor, tokenizer = create_model_and_transforms(
     clip_vision_encoder_pretrained="openai",
     lang_encoder_path=MODEL["language"],
     tokenizer_path=MODEL["language"],
-    cross_attn_every_n_layers=4
+    cross_attn_every_n_layers=MODEL["cross_attn_every_n_layers"],
 )
 
 # grab model checkpoint from huggingface hub
@@ -82,7 +89,7 @@ Details: In the text we expect an <image> special token to indicate where an ima
 """
 tokenizer.padding_side = "left" # For generation padding tokens should be on the left
 lang_x = tokenizer(
-    ["<image>An image of two cats.<|endofchunk|><image>An image of a bathroom sink.<|endofchunk|><image>An image of"],
+    ["<image>An image of two dogs.<|endofchunk|><image>An image of a basketball.<|endofchunk|><image>An image of"],
     return_tensors="pt",
 )
 
