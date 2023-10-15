2eeb9dfb8711ac4c00abf9848491582360d9c76e
shuochen <shuo.chen@campus.lmu.de>
2023-10-15 23:03:05 +0200

    feat: find by rankning

--- open_flamingo/eval/rices.py
+++ open_flamingo/eval/rices.py
@@ -3,7 +3,10 @@ import torch
 from tqdm import tqdm
 import torch
 from utils import custom_collate_fn
+from sentence_transformers import SentenceTransformer
+import logging
 
+logger = logging.getLogger(__name__)
 
 class RICES:
     def __init__(
@@ -11,7 +14,7 @@ class RICES:
         dataset,
         device,
         batch_size,
-        vision_encoder_path="ViT-B-32",
+        vision_encoder_path="ViT-L-14",
         vision_encoder_pretrained="openai",
         cached_features=None,
     ):
@@ -26,6 +29,8 @@ class RICES:
         )
         self.model = vision_encoder.to(self.device)
         self.image_processor = image_processor
+
+        self.text_model = SentenceTransformer('sentence-transformers/sentence-t5-base').to(self.device)
 
         # Precompute features
         if cached_features is None:
@@ -94,3 +99,120 @@ class RICES:
 
         # Return with the most similar images last
         return [[self.dataset[i] for i in reversed(row)] for row in indices]
+
+    def find_every_nth(self, batch, num_examples, n=8):
+        """
+        Get the top num_examples most similar examples to the images.
+        """
+        # Switch to evaluation mode
+        self.model.eval()
+
+        with torch.no_grad():
+            inputs = torch.stack([self.image_processor(image) for image in batch]).to(
+                self.device
+            )
+
+            # Get the feature of the input image
+            query_feature = self.model.encode_image(inputs)
+            query_feature /= query_feature.norm(dim=-1, keepdim=True)
+            query_feature = query_feature.detach().cpu()
+
+            if query_feature.ndim == 1:
+                query_feature = query_feature.unsqueeze(0)
+
+            # Compute the similarity of the input image to the precomputed features
+            similarity = (query_feature @ self.features.T).squeeze()
+
+            if similarity.ndim == 1:
+                similarity = similarity.unsqueeze(0)
+
+            # Get the indices of the 'num_examples' most similar images
+            indices = similarity.argsort(dim=-1, descending=True)[:, :num_examples*n]
+
+        # Return with the most similar images last
+        return [[self.dataset[i] for i in reversed(row)[::n]] for row in indices]
+
+
+    def find_by_ranking_similar_text(self, batch_image, batch_text, num_examples):
+        """
+        RICES Images -> rank based on text similarity
+
+        Args:
+            batch ():
+            num_examples ():
+
+        Returns:
+
+        """
+        self.model.eval()
+
+        with torch.no_grad():
+            inputs = torch.stack([self.image_processor(image) for image in batch_image]).to(
+                self.device
+            )
+
+            # Get the feature of the input image
+            query_feature = self.model.encode_image(inputs)
+            query_feature /= query_feature.norm(dim=-1, keepdim=True)
+            query_feature = query_feature.detach().cpu()
+
+            if query_feature.ndim == 1:
+                query_feature = query_feature.unsqueeze(0)
+
+
+            # logger.debug(f"query_feature shape: {query_feature.shape}")
+            # logger.debug(f"self.features shape: {self.features.shape}")
+            # Compute the similarity of the input image to the precomputed features
+            similarity = (query_feature @ self.features.T).squeeze()
+
+            if similarity.ndim == 1:
+                similarity = similarity.unsqueeze(0)
+            # logger.debug(f"similarity shape: {similarity.shape}")
+            # Get the indices of the 'num_examples' most similar images
+            indices = similarity.argsort(dim=-1, descending=True)[:, :200] # TODO, top-100
+            rices_samples = [[self.dataset[i] for i in reversed(row)] for row in indices]
+            # indices = similarity.argsort(dim=-1, descending=True)[:, :num_examples]
+            # return [[self.dataset[i] for i in reversed(row)] for row in indices]
+
+            # rank based on text similarity
+
+            text_inputs = [text for text in batch_text]
+            # logger.debug(f"text_inputs: {text_inputs}")
+            text_query_features = self.text_model.encode(
+                text_inputs,
+                convert_to_tensor=True,
+                show_progress_bar=False,
+            )
+            text_query_features /= text_query_features.norm(dim=-1, keepdim=True)
+
+            if self.dataset.dataset_name in ["vqav2", "ok_vqa", "vizwiz", "gqa", "textvqa"]:
+                rices_samples_text = [[sample["question"] for sample in samples] for samples in rices_samples]
+            elif self.dataset.dataset_name in ["coco", "flickr"]:
+                rices_samples_text = [[sample["caption"] for sample in samples] for samples in rices_samples]
+            else:
+                raise NotImplementedError(f"dataset_name: {self.dataset.dataset_name} not supported")
+
+            rices_samples_text_features = torch.stack([self.text_model.encode(
+                sample_text,
+                convert_to_tensor=True,
+                show_progress_bar=False,
+            ) for sample_text in rices_samples_text])
+
+            rices_samples_text_features /= rices_samples_text_features.norm(dim=-1, keepdim=True)
+            text_query_features = text_query_features.unsqueeze(dim=1)
+            # logger.debug(f"rices_samples_text_features.shape: {rices_samples_text_features.shape}"
+            #              f"text_query_features.shape: {text_query_features.shape}")
+            # text_similarity = (text_query_features @ rices_samples_text_features.T).squeeze()
+            text_similarity = torch.einsum("bij,bkj->bki", text_query_features, rices_samples_text_features)
+            text_similarity = text_similarity.squeeze(dim=-1)
+            # logger.debug(f"text_similarity.shape: {text_similarity.shape}")
+            indices = text_similarity.argsort(dim=-1, descending=True)[:, :num_examples] # TODO,
+
+            # demos = [[rices_samples[j][i] for i in reversed(row)] for j,row in enumerate(indices)]
+            # sub_ind = torch.randperm(indices.shape[1])
+            # indices = indices[:, sub_ind[:num_examples]]
+            # logger.debug(f"indices.shape: {indices.shape}")
+            # assert False
+        return [[rices_samples[j][i] for i in row] for j,row in enumerate(indices)]
+
+
