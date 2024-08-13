123be93ef49ca96d50749956da0911120fe58f87
shuochen <shuo.chen@campus.lmu.de>
2024-08-13 21:14:35 +0200

    script: evaluate baseline ProL on base class using media 4 text per 3

--- open_flamingo/attention_guidance/scripts/run.sh
+++ open_flamingo/attention_guidance/scripts/run.sh
@@ -1,5 +1,5 @@
-
-data=('r' 'c' 's' 'a')
+## evaluate baseline ProL on base class
+data=('1k' 'v2' 'r' 'c' 's' 'a')
 class=('8' '16' '32')
 
 for dataset in "${data[@]}"; do
@@ -8,3 +8,16 @@ for dataset in "${data[@]}"; do
     python main.py evaluate_dataset=imagenet-$dataset load_from=baseline-class-$load-m4-t3 > ./logs/imagenet-$dataset-class-$load-m4-t3.log 2>&1
   done
 done
+
+# evaluate baseline ProL on novel class
+data=('1k' 'v2' 'r' 'c' 's' 'a')
+class=('8')
+for dataset in "${data[@]}"; do
+  for load in "${class[@]}"; do
+    echo "Running imagenet-$dataset-class-$load-m4-t3"
+    python main.py evaluate_dataset=imagenet-$dataset load_from=baseline-class-$load-m4-t3 eval_novel_classes=True > ./logs/imagenet-$dataset-class-$load-novel-class-m4-t3.log 2>&1
+  done
+done
+
+
+#python main.py evaluate_dataset=imagenet-1k load_from=robust-try eval_novel_classes=False device=cuda:1
